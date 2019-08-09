47b0abc33272dd492254ec72424f2d66dcc7d32e
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-09 19:48:22 -0300

    placing the mat-accent on the active button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { Routes, RouterModule } from '@angular/router'
 const appRoutes: Routes = [
   { path:'dvds', component: DvdComponent},
   { path:'books', component: BookComponent},
+  { path:'', pathMatch: 'full', redirectTo: 'dvds'},
+  { path:'**', component: PageNotFoundComponent}
 ]
 
 @NgModule({
