0ef5c50372a888e0cf58e1e064ec92d05aba762d
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-10 12:00:18 -0300

    getting book details

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,6 +1,9 @@
+import { Book } from './../../models/book';
 import { BookService } from './../../services/book.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-detail',
@@ -9,15 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailComponent implements OnInit {
 
+  book$: Observable<Book> = null;
+  index: number;
+
   constructor(
     private route: ActivatedRoute,
-    private bookService: BookService
+    private bookService: BookService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     console.log('Index: ', this.route.snapshot.paramMap.get('index'));
-    this.route.paramMap
-    .subscribe((params: ParamMap) => console.log("Index: ", params.get('index')));
+    this.book$ = this.route.paramMap
+      .pipe(
+        tap((params: ParamMap) => this.index = +params.get('index')),
+        switchMap((params: ParamMap) => this.bookService.get(+params.get('index'))));
+    //.subscribe((params: ParamMap) => console.log("Index: ", params.get('index')));
+  }
+
+  remove() {
+    this.bookService.remove(this.index);
+    this.router.navigate(['books']);
   }
 
 }
